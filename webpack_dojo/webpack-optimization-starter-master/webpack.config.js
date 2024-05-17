1258d306a0ac588f9da3a6e22c566cbf6419bb20
Kevin Andres Ortiz Merchan <kevinandresortizmerchan@outlook.com>
2024-05-17 11:38:40 -0500

    [ROOM-TIME]

---
+++ webpack_dojo/webpack-optimization-starter-master/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+const config = {
+    entry: './src',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    mode: 'development'
+
+}
+
+module.exports = config;
