b4d1484dbfd4b2b5cfba75c95312123bcca0350a
Kevin Andres Ortiz Merchan <kevinandresortizmerchan@outlook.com>
2024-05-20 10:24:06 -0500

    [WEBPACK]|[DOJO]| Setting up webpack Dev Server

--- webpack_dojo/webpack-optimization-starter-master/webpack/webpack.dev.config.js
+++ webpack_dojo/webpack-optimization-starter-master/webpack/webpack.dev.config.js
@@ -1,6 +1,21 @@
 const common = require('./webpack.common.config');
+const path = require('path');
 const { merge } = require('webpack-merge');
 
 module.exports = merge(common, {
-    mode: 'development'
+    mode: 'development',
+    devServer: {
+        port: 9000,
+        static:{
+            directory: path.resolve(__dirname,'..')
+        },
+        devMiddleware: {
+            index: 'index.html',
+            writeToDisk: true
+        },
+        client: {
+            overlay: true
+        },
+        liveReload: false
+    }
 })
